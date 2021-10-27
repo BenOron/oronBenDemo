21f0d2145acd3a86b70a36f92b675b6b87f0f2f1
i075528 <ben.oron@sap.com>
2021-10-27 17:14:48 +0300

    fix save rew

--- routes/api.js
+++ routes/api.js
@@ -18,54 +18,12 @@ router.get('/widgets', (req, res) => {
 });
 
 
-router.put('/widgets/update/:id', async function (req, res) {
-    const _id = req.params.id  //pass ID user
-    Widget.updateOne(_id, {
-        $set: {
-            "title": req.body.title,
-            "brandName": req.body.brandName,
-            "country": req.body.country,
-            "imgSrcUri": req.body.imgSrcUri,
-            "locationWidget": req.body.locationWidget
-        }
-    }, (err) => {
-        if (err) return res.status(200).json({
-            error: true,
-            code: 115,
-            message: "Erro to update user!"
-        })
-    })
-})
-
-
-// router.put('/update/:id', function (req, res) {
-//     Widget.findById(req.params.id, (err, responseByID) => {
-//         responseByID.updateOne(req.body, (err) => {
-//             if (err) {
-//                 res.status(500).send(err);
-//             }
-//             Widget.find((err) => {
-//                 if (err) {
-//                     res.status(500).send(err)
-//                 }
-//
-//                 res.json({
-//                     success: true,
-//                     message: 'Board update successfully'
-//                 });
-//             });
-//         });
-//     });
-// });
-
-
 
 
 router.post('/save', (req, res) => {
     const data = req.body;
     const widgets = new Widget(data);
-
-    widgets.save((error) => {
+    Widget.save((error) => {
         if (error) {
             res.status(500).json({ msg: 'Sorry, internal server errors' });
             return;
