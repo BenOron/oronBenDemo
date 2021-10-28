0df10f3c9516f5b71da7c079fd4d700bb98e9e23
i075528 <ben.oron@sap.com>
2021-10-28 13:37:07 +0300

    Test Example

--- tabApp/src/App.test.js
+++ tabApp/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Home from '../src/component/Home';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders home', () => {
+  render(<Home />);
+  const taboolaAlt = screen.getByTitle('Edit Mode')
+  expect(taboolaAlt).toBeInTheDocument();
 });
+
